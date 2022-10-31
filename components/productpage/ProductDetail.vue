2c55e5100c80df8ef016017c0f146e008fbbae8c
YolradeeIT <64070089@kmitl.ac.th>
2022-10-31 21:33:05 +0700

    nah it is new icon

---
+++ components/productpage/ProductDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="inline">
+        <div>
+            <img src="https://www.jib.co.th/img_master/product/medium/2022052309492353368_1.jpg?v=533681653363436"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- pages/product.vue
+++ pages/product.vue
@@ -1,9 +1,16 @@
 <template>
-    
+    <div>
+        <product-detail></product-detail>
+    </div>
 </template>
 
 <script>
-
+    import ProductDetail from '~/components/productpage/ProductDetail.vue'
+    export default {
+        name:"ProductPage",
+        components: { ProductDetail }
+        
+    }
 </script>
 
 <style>
