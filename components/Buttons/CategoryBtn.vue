045f49c91b366c706018683ae103b2c2f97b8851
Poom <68049473+poompongphun@users.noreply.github.com>
2022-10-31 22:17:31 +0700

    feat: 🎸 add category btn

---
+++ components/Buttons/CategoryBtn.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    class="
+      aspect-video
+      bg-gradient-to-t
+      from-primary
+      to-lightviolet
+      max-w-[200px]
+      w-full
+      rounded-lg
+      flex
+      justify-center
+      items-center
+      shadow-lg
+      hover:scale-95
+      active:scale-90
+      transition-all
+    "
+    @click="$router.push('/category/' + name)"
+  >
+    <div class="text-center text-md font-bold text-white">
+      <div>
+        <FontAwesomeIcon :icon="icon" class="text-3xl" />
+      </div>
+      <div>{{ text }}</div>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CategoryBtn',
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     <ad-slider class="my-8" :images="images" />
-    <div>
+    <div class="mb-8">
       <div class="text-2xl font-bold">หมวดหมู่สินค้า</div>
+      <div
+        class="my-2 flex flex-wrap justify-start gap-2"
+        :class="{ 'justify-center': category.length > 7 }"
+      >
+        <category-btn
+          v-for="(item, i) in category"
+          :key="i"
+          :icon="item.icon"
+          :text="item.text"
+          :name="item.name"
+        />
+      </div>
     </div>
-    <div>
+    <div class="mb-8">
       <div class="text-2xl font-bold">สินค้าแนะนำ</div>
-      <div class="grid grid-cols-5 gap-4">
-        <product-card v-for="(pif, i) in productinfo" :key="i" :data="pif"></product-card>
+      <div class="grid grid-cols-5 gap-4 my-2">
+        <product-card
+          v-for="(pif, i) in productinfo"
+          :key="i"
+          :data="pif"
+        ></product-card>
       </div>
     </div>
   </div>
@@ -15,15 +31,43 @@
 
 <script>
 import AdSlider from '~/components/Swiper/AdSlider.vue'
+import CategoryBtn from '~/components/Buttons/CategoryBtn.vue'
 export default {
   name: 'IndexPage',
-  components: { AdSlider },
+  components: { AdSlider, CategoryBtn },
   data: () => ({
     images: [
       'https://www.jib.co.th/img_master/banner/original/20221012101403_1.jpg?v=33001665544459',
       'https://www.jib.co.th/img_master/banner/original/20221026114132_1.jpg?v=33271666759302',
       'https://www.jib.co.th/img_master/banner/original/20221025115530_1.jpg?v=33211666687032',
       'https://www.jib.co.th/img_master/banner/original/20221026153622_1.jpg?v=33291666773856',
+    ],
+    category: [
+      {
+        icon: 'headphones',
+        name: 'headphones',
+        text: 'หูฟัง',
+      },
+      {
+        icon: 'microphone',
+        name: 'microphone',
+        text: 'ไมโครโฟน',
+      },
+      {
+        icon: 'display',
+        name: 'graphic',
+        text: 'การ์ดจอ',
+      },
+      {
+        icon: 'computer-mouse',
+        name: 'mouse',
+        text: 'เมาส์',
+      },
+      {
+        icon: 'laptop',
+        name: 'laptop',
+        text: 'โน๊ตบุ๊ค',
+      },
     ],
     productinfo: [
       {
@@ -85,8 +129,8 @@ export default {
         ],
         description:
           'Short detail : VGA (การ์ดแสดงผล) MSI GEFORCE RTX 3090 TI SUPRIM X 24G - 24GB GDDR6X',
-      }
-    ]
+      },
+    ],
   }),
 }
 </script>
