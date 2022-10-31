7e6129269ccce85a607e7c3c7a380740211e85c9
DevNebulaP <puttipong.nebula@gmail.com>
2022-10-31 17:10:13 +0700

    Added MainFooter
    Initial commit to the main footer

--- components/Layouts/MainFooter.vue
+++ components/Layouts/MainFooter.vue
@@ -1,5 +1,78 @@
 <template>
-  <div>Footer</div>
+  <div
+    class="
+      w-100
+      bg-primary
+      h-[19rem]
+      rounded-t-3xl
+      mx-2
+      px-10
+      pt-10
+      mt-10
+      "
+  >
+    <div
+      class="
+        w-full
+        bg-[rgba(255,255,255,0.7)]
+        h-full
+        rounded-t-3xl
+        p-5
+        "
+    >
+      <div
+        class="
+          container
+          h-full
+          mx-auto
+          flex
+          "
+      >
+        <div
+          class="
+            w-64
+            text-primary
+            flex-none
+            flex
+            items-center
+            mr-5
+          "
+        >
+          <div>
+            <img class="w-1/2" src="/logo.png" alt="logo"/>
+            <span>หมาร้องโฮ่ง แมวร้องเหมียว นกร้องจิ๊บ ส่วนหนูร้องจิ๊ด</span>
+          </div>
+        </div>
+        <div
+          class="
+            text-primary
+            flex-auto
+            "
+        >
+          <span class="text-xl">Meet our team</span>
+          <div 
+            class="
+              w-100
+            "
+          >
+
+          </div>
+        </div>
+        <div
+          class="
+            bg-black
+            flex-none
+            rounded-xl
+            w-48
+            ml-5
+            text-white
+            "
+        >
+          <span>View Github</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
