67be67ad6c7d7da4dcd257c14cafa421fb222e64
Poom <68049473+poompongphun@users.noreply.github.com>
2022-11-03 02:25:37 +0700

    fix: 🐛 fix radio

---
+++ components/Checkout/CheckoutDetail.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="fixed bottom-4 w-full left-0">
+    <div
+      class="
+        bg-primary
+        w-full
+        h-40
+        p-10
+        container
+        mx-auto
+        rounded-lg
+        flex
+        justify-between
+        items-center
+        shadow-lg
+      "
+    >
+      <div class="flex justify-center items-center gap-10 text-white">
+        <div class="text-sm">
+          จำนวน
+          <div class="text-xl">4 ชิ้น</div>
+        </div>
+        <div class="text-sm">
+          ราคาทั้งหมด
+          <div class="text-xl">200,000 บาท</div>
+        </div>
+      </div>
+      <button
+        class="
+          max-w-[270px]
+          w-full
+          h-14
+          text-primary
+          bg-white
+          rounded-md
+          font-bold
+        "
+      >
+        เลือกที่อยู่
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
