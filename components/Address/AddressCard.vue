d156568a7cf5ac111fc548ed324947b3439ad34c
Poom <68049473+poompongphun@users.noreply.github.com>
2022-11-02 22:48:15 +0700

    feat: 🎸 address del btn

--- components/Address/AddressCard.vue
+++ components/Address/AddressCard.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="bg-white rounded-lg shadow-md aspect-[16/7] p-4 h-full w-full">
+  <div
+    class="
+      bg-white
+      rounded-lg
+      shadow-md
+      aspect-[16/7]
+      p-4
+      h-full
+      w-full
+      relative
+    "
+  >
     <div class="text-center font-bold">{{ data.title }}</div>
     <div class="mt-2 text-stone-400">
       <span class="font-bold text-black">
@@ -11,6 +22,9 @@
         {{ data.address }}
       </div>
     </div>
+    <button v-if="editable" class="absolute top-0 right-0 m-2 aspect-square w-7 h-7" @click="$store.commit('address/removeAddress', data)">
+      <FontAwesomeIcon icon="trash" class="text-red-500" />
+    </button>
   </div>
 </template>
 
@@ -21,6 +35,10 @@ export default {
       type: Object,
       default: () => ({}),
       required: true,
+    },
+    editable: {
+      type: Boolean,
+      default: false,
     },
   },
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -17,6 +17,7 @@ export default {
   components: { MainFooter, NavBar },
   mounted() {
     this.$store.commit('init')
+    this.$store.commit('address/init')
   },
 }
 </script>
