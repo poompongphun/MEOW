844b55276b1636b76e5841da1d61b055830873d8
Poom <68049473+poompongphun@users.noreply.github.com>
2022-11-03 02:26:15 +0700

    fix: 🐛 fix myradio

--- pages/chooseAddress.vue
+++ pages/chooseAddress.vue
@@ -8,56 +8,17 @@
         </template>
       </my-radio>
     </div>
-    <div class="fixed bottom-4 w-full left-0">
-      <div
-        class="
-          bg-primary
-          w-full
-          h-40
-          p-10
-          container
-          mx-auto
-          rounded-lg
-          flex
-          justify-between
-          items-center
-          shadow-lg
-        "
-      >
-        <div class="flex justify-center items-center gap-10 text-white">
-          <div class="text-sm">
-            จำนวน
-            <div class="text-xl">4 ชิ้น</div>
-          </div>
-          <div class="text-sm">
-            ราคาทั้งหมด
-            <div class="text-xl">200,000 บาท</div>
-          </div>
-        </div>
-        <button
-          class="
-            max-w-[270px]
-            w-full
-            h-14
-            text-primary
-            bg-white
-            rounded-md
-            font-bold
-          "
-        >
-          เลือกที่อยู่
-        </button>
-      </div>
-    </div>
+    <checkout-detail />
   </div>
 </template>
 
 <script>
 import MyRadio from '~/components/Checkout/MyRadio.vue'
 import AddressCard from '~/components/Address/AddressCard.vue'
+import CheckoutDetail from '~/components/Checkout/CheckoutDetail.vue'
 
 export default {
-  components: { MyRadio, AddressCard },
+  components: { MyRadio, AddressCard, CheckoutDetail },
   layout: 'noFooter',
   data: () => ({
     selected: null,
