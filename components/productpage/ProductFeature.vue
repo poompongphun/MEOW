f6d885e3afd73ad488602f21c0404f68eb88b1fd
YolradeeIT <64070089@kmitl.ac.th>
2022-11-12 23:16:17 +0700

    edit

--- components/productpage/ProductFeature.vue
+++ components/productpage/ProductFeature.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="(feature, i) in data" :key="i" class="rounded-lg bg-primary p-5 mt-5">
-            <p class="text-white text-xl">{{ feature.name }}</p>
+            <p class="text-white text-xl mb-2">{{ feature.name }}</p>
             <div v-for="(detail, j) in feature.detail" :key="j" class="flex flex-row bg-white rounded-lg" :class="{'mb-2': (j+1 !== feature.detail.length)}">
                 <div class="uppercase bg-lightBlue p-2 rounded-lg font-medium" style="flex-basis: 10%">{{ detail.key }}</div>
                 <div class="p-2">{{ detail.value }}</div>
